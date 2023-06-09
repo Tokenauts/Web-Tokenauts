1b03eed65a338bef920f098f6bb39276d3217c44
Tokenauts <Tokenauts@gmail.com>
2023-06-09 12:04:51 +0530

    Initial Commit

---
+++ src/Components/HeroVideo.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { Home } from "../assets";
+import { motion } from "framer-motion";
+const HeroVideo = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 8000); // 5000 milliseconds = 5 seconds
+
+    return () => clearTimeout(timer); // Clean up timer on unmount
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [loading]);
+
+  return (
+    <div
+      className="flex justify-center items-center h-screen -mt-16 z-0"
+      style={{
+        pointerEvents: loading ? "none" : "auto",
+      }}
+    >
+      <video className="absolute z-0 w-auto " src={Home} autoPlay muted />
+
+      <div className="flex flex-col items-center z-10 text-white ">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 8 }}
+        >
+          <div className=" font-Orbitron font-bold text-center text-5xl ">
+            Navigate the Cosmos
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 8 }}
+        >
+          <div className=" font-Orbitron font-bold text-center mt-2 text-5xl">
+            of Investment Opportunities
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2, delay: 9 }}
+        >
+          <div className=" font-Orbitron  text-center mt-20 lg:text-base	">
+            Scroll Down{" "}
+          </div>
+        </motion.div>
+      </div>
+      {/* Content goes here */}
+    </div>
+  );
+};
+
+export default HeroVideo;
---
+++ src/Components/nav.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+
+
+import logoVideo from "../assets/logo.mp4";
+const Nav = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, delay: 3 }}
+    >
+      <nav>
+        <div class=" relative first-line:placeholder:max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 max-w-5xl z-50	 ">
+          <video autoPlay loop muted playsInline className=" h-28">
+            // <source src={logoVideo} type="video/mp4" />
+            //{" "}
+          </video>
+
+          <div class="flex md:order-2">
+           <a href="https://3kw0tru8iwz.typeform.com/to/XxsmEeO0">
+              <button
+                type="button"
+                class="text-black bg-[#99EDCC] hover:bg-#e5eeea focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-#99EDCC dark:hover:bg-#fdfdfd dark:focus:ring-white"
+              >
+                Join Waitlist
+              </button>
+            </a>
+            {/* <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button> */}
+          </div>
+          {/* <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white rounded hover:bg-#99EDCC md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-#99EDCC dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div> */}
+        </div>
+      </nav>
+    </motion.div>
+  );
+};
+
+export default Nav;
